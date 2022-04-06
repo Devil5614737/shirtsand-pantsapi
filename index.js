0958fb666b511f1dd914fe14ff6a3e4d4eb2f7d9
kaushik <kaushiksheel9@gmail.com>
2022-04-06 17:09:30 +0530

    adding quantity

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ const shirts = [
     ],
     image:
       "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/59/9601/1-fashion_catalog.jpg",
+    quantity:1
+      
   },
   {
     id: 2,
@@ -36,8 +38,11 @@ const shirts = [
       "1 Casual Shirts",
     ],
     image:
+
       "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/97/538/1-fashion_catalog.jpg",
+      quantity:1
   },
+  
   {
     id: 3,
     name: "Knighthood Solid Mens Formal Shirts",
@@ -51,6 +56,9 @@ const shirts = [
     ],
     image:
       "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/91/6301/1-fashion_catalog.jpg",
+      quantity:1
+
+      
   },
   {
     id: 4,
@@ -65,6 +73,9 @@ const shirts = [
     ],
     image:
       "https://cflare.shop.bigbazaar.com/cdn-cgi/image/width=450/media/product/32/638/1-fashion_catalog.jpg",
+      quantity:1
+
+      
   },
   {
     id: 5,
@@ -74,29 +85,38 @@ const shirts = [
     easyInfo: ["Made of Pure Cotton", "Full Sleeve"],
     image:
       "https://rukminim2.flixcart.com/image/800/960/kk76wsw0/shirt/x/s/l/xl-st408nb-majestic-man-original-imafzhwgn4uggtzy.jpeg?q=50",
+      quantity:1
   },
+  
   {
     id: 6,
     name: "Men Regular Fit Checkered Casual Shirt",
     sizes: ["S", "M", "L", "XL"],
+
     price: 35.83,
     easyInfo: ["Made of Pure Cotton", "Full Sleeve"],
     image:
       "https://rukminim2.flixcart.com/image/800/960/kcnp8y80/shirt/7/2/a/38-11326694-roadster-original-imaftqf573vvrzag.jpeg?q=50",
+      quantity:1
   },
+  
   {
     id: 7,
     name: "TOMMY HILFIGER Men Regular Fit Houndstooth Casual Shirt",
     sizes: ["S", "M", "L", "XL"],
+
     price: 65.12,
     easyInfo: ["Full Sleeve", "Stripped Texture"],
     image:
       "https://rukminim2.flixcart.com/image/800/960/kuzuoi80/shirt/c/t/j/m-a1atw218-tommy-hilfiger-original-imag7ztvf3kgy6tm.jpeg?q=50",
+      quantity:1
   },
+  
   {
     id: 8,
     name: "Dennis Lingo Men's Checkered Slim fit Casual Shirt ",
     sizes: ["S", "M", "L", "XL", "2XL"],
+
     price: 9.2,
     easyInfo: [
       "100% Premium Cotton, Pre-Washed for extremely soft finish and Rich look ",
@@ -105,7 +125,10 @@ const shirts = [
       "Stylish checkered full sleeve cotton casual shirt",
     ],
     image: "https://m.media-amazon.com/images/I/61j0cpBJjJS._UX679_.jpg",
+    quantity:1
   },
+
+  
   {
     id: 9,
     name: "CAVALLO by Linen Club Blue Printed Regular Fit Casual Shirts ",
@@ -118,7 +141,10 @@ const shirts = [
       "Stylish checkered full sleeve cotton casual shirt",
     ],
     image: "https://m.media-amazon.com/images/I/61jcgJZu+xL._UX679_.jpg",
+    quantity:1
   },
+
+  
   {
     id: 10,
     name: "Diverse Men's Slim Casual Shirt",
@@ -130,8 +156,11 @@ const shirts = [
       "Conceptualized and created completely in India ",
     ],
     image: "https://m.media-amazon.com/images/I/512pBZ7L-JL._UX679_.jpg",
+    quantity:1
   },
   {
+    
+
     id: 11,
     name: "Amazon Brand - Inkast Denim Co. Men's Slim Fit Casual Shirt ",
     sizes: ["S", "M", "L", "XL", "2XL"],
@@ -143,7 +172,10 @@ const shirts = [
       "Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm Iron ",
     ],
     image: "https://m.media-amazon.com/images/I/81EldXaltNL._UX569_.jpg",
+    quantity:1
   },
+
+  
   {
     id: 12,
     name: "GANT Men Blue Color Block Shirts ",
@@ -151,8 +183,10 @@ const shirts = [
     price: 124.99,
     easyInfo: ["Fabric - 100% Cotton ", "Long sleeve Shirt  "],
     image: "https://m.media-amazon.com/images/I/61vhNV3eYtL._UX679_.jpg",
+    quantity:1
   },
 ];
+
 
 const pants = [
   {
@@ -162,7 +196,9 @@ const pants = [
     sizes: [28, 30, 32, 36, 38, 40.42],
     image: "https://m.media-amazon.com/images/I/81q4Lx8JsVL._UX569_.jpg",
     desc: "",
+    quantity:1
   },
+  
   {
     id: 2,
     name: "Levi's Men's Regular Jeans ",
@@ -170,7 +206,9 @@ const pants = [
     sizes: [28, 30, 32, 36, 38, 40.42],
     image: "https://m.media-amazon.com/images/I/81cf1ZNl3qL._UX569_.jpg",
     desc: "",
+    quantity:1
   },
+  
   {
     id: 3,
     name: "Slim Men Black Jeans",
@@ -179,6 +217,8 @@ const pants = [
     image:
       "https://rukminim2.flixcart.com/image/800/960/kfcv6vk0-0/jean/4/w/m/30-ud0014-30-united-denim-original-imafvu2chpkpkeuv.jpeg?q=50",
     desc: "",
+    quantity:1
+    
   },
   {
     id: 4,
@@ -188,6 +228,8 @@ const pants = [
     image:
       "https://rukminim2.flixcart.com/image/800/960/l0tweq80/jean/0/a/9/32-lmjn001594-lee-original-imagcgzmbdgyastr.jpeg?q=50",
     desc: "",
+    quantity:1
+    
   },
   {
     id: 5,
@@ -197,6 +239,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/670177/06/mod01/fnd/IND/fmt/png/PUMA-Graphic-Men's-Pants",
     desc: "These PUMA Graphic Men's pants are built for all-purpose training. While the moisture wicking fabric keeps you comfortable, the PUMA branding elevates your street and casual style.",
+    quantity:1
+    
   },
   {
     id: 6,
@@ -206,6 +250,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/586731/01/mod01/fnd/IND/fmt/png/Active-Tricot-Slim-Fit-Men's-Sweat-Pants",
     desc: "High-performance apparel for the ambitious athlete: these stylish bottoms combine the moisture-wicking power of dryCELL and miDori® bioWick to help you reach your peak.",
+    quantity:1
+    
   },
   {
     id: 7,
@@ -215,6 +261,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/586731/01/mod01/fnd/IND/fmt/png/Active-Tricot-Slim-Fit-Men's-Sweat-Pants",
     desc: "High-performance apparel for the ambitious athlete: these stylish bottoms combine the moisture-wicking power of dryCELL and miDori® bioWick to help you reach your peak.",
+    quantity:1
+    
   },
   {
     id: 8,
@@ -224,6 +272,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/532348/04/mod01/fnd/IND/fmt/png/Mercedes-F1-Essential-Regular-Fit-Men's-Pants",
     desc: "Bring a touch of Mercedes-AMG Petronas Motorsport to your everyday essential wear. Made from sustainably sourced and recycled fabrics, these co-branded French terry sweats are a must-have for Formula 1 fans who like to keep their look low-key and casual. They're also made using cotton from the Forever Better Initiative, and contain recycled material – so not only will you look great, you'll feel great, too.",
+    quantity:1
+    
   },
   {
     id: 9,
@@ -233,6 +283,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/596146/02/mod01/fnd/IND/fmt/png/Scuderia-Ferrari-Men's-Sweat-Pants",
     desc: "Simplicity is key with these sweatpants. With plenty of room to move, they have an elasticated waistband with a drawcord so you can create the exact fit you want. All while celebrating the Scuderia Ferrari racing team, one of motorsport's most famous and iconic teams.",
+    quantity:1
+    
   },
   {
     id: 10,
@@ -242,6 +294,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/599509/01/mod01/fnd/IND/fmt/png/BMW-M-Motorsport-Knitted-T7-Men's-Slim-SweatPants",
     desc: "A must-have for PUMA and BMW M Motorsport fans alike. We've borrowed design features from the original T7 heritage style and added modern motorsport elements to bring you these sleek, slim-fitting double-knit sweatpants, with the iconic T7 stripe in the classic colours of BMW M Motorsport.",
+    quantity:1
+    
   },
   {
     id: 11,
@@ -251,6 +305,8 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/579340/02/mod01/fnd/IND/fmt/png/PUMA-x-Virat-Kohli-Men's-Sweat-Pants",
     desc: "",
+    quantity:1
+    
   },
   {
     id: 12,
@@ -260,8 +316,10 @@ const pants = [
     image:
       "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/585753/03/mod01/fnd/IND/fmt/png/Rebel-Men's-Pants",
     desc: "Everyone needs a reliable and comfortable pair of pants in their wardrobe. And whether you're working out or just hanging out, these will do the job. There's a ribbed waistband with internal drawcords, ribbed cuffs and side seam pockets for your belongings. Taken from our Rebel Collection, these pants are made with cotton from the Forever Better Initiative, which means just by buying them you're supporting more sustainable cotton farming. So, you can feel good about looking good.",
+    quantity:1
   },
 ];
+
 
 app.get("/", (req, res) => {
   res.status(200).send("welcome to clothing api");
